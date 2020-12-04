47cfa5e80236ee1da21e09aeea28986e57cf79d5
Ajerzz <sergeysagarunyan@mail.ru>
2020-12-04 20:40:29 +0300

    load

--- src/views/Todos.vue
+++ src/views/Todos.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-  
-     <h2> 
-       Доступные вакансии
-     </h2>
-    
+    <h2>
+      Доступные вакансии
+    </h2>
     <hr>
-    <select v-model="filter">
+    <select v-model="filterRegion">
       <option value="Выберите район">Выберите район</option>
       <option value="Ворошиловский">Ворошиловский</option>
       <option value="Железнодорожный">Железнодорожный</option>
@@ -18,8 +16,8 @@
       <option value="Советский">Советский</option>
     </select>
     <hr>
-    <select v-model="filter2">
-      <option value="Выберите количество работников">Выберите количество работников</option>
+    <select v-model="filterWorkers">
+      <option value="Выберите количество человек">Выберите количество человек</option>
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
@@ -29,111 +27,51 @@
     <hr>
     <h2>Список вариантов</h2>
     <Loader v-if="loading" />
-   
     <TodoList
         v-else-if="filteredTodos.length"
         v-bind:todos="filteredTodos"
         @remove-todo="removeTodo"
     /> <!--Dobavit filter filteredTodos2 -->
     <p v-else>Извините, мы не смогли найти подходящие варианты</p>
-   <li>Чтобы скрыть ненужные варианты вы можете нажать на крестик справа от объявления. 
-        
-    <router-link to="/">Выйти на главную</router-link>
-          <h4>
-    <img src="./HH.jpg" alt="альтернативный текст" width="400" height="300">
-    </h4>
-    
-   </li>
+    <li>Чтобы скрыть ненужные варианты вы можете нажать на крестик справа от объявления.
+      <router-link to="/">Выйти на главную</router-link>
+      <h4>
+        <img src="./HH.jpg" alt="альтернативный текст" width="400" height="300">
+      </h4>
+    </li>
   </div>
- 
 </template>
 
 <script>
 import TodoList from '@/components/TodoList'
 import AddTodo from '@/components/AddTodo'
 import Loader from '@/components/Loader'
+
 export default {
   name: 'app',
   data() {
     return {
       todos: [
-      {id: 0, title: "Требуется грузчик", rayon: "Ворошиловский", workers: 4 },
-      {id: 1, title: "Найму выгульщика для собак", rayon: "Октябрьский", workers: 2},
-      {id: 2, title: "Ищу специалиста в ремонте", rayon: "Кировский", workers: 1},
-      {id: 3, title: "Требуется слесарь", rayon: "Железнодорожный", workers: 1},
-      {id: 4, title: "Требуется сварщик", rayon: "Ворошиловский"  , workers: 3},
-      {id: 5, title: "Требуется Сиделка", rayon: "Октябрьский", workers: 4},
-      {id: 6, title: "Ищу бригаду строителей ", rayon: "Советский", workers: 3},
-      {id: 7, title: "Требуется специалист по клинингу", rayon: "Железнодорожный", workers: 2}]
+        { id: 0, title: "Требуется грузчик", rayon: "Ворошиловский", workers: 4 },
+        { id: 1, title: "Найму выгульщика для собак", rayon: "Октябрьский", workers: 2 },
+        { id: 2, title: "Ищу специалиста в ремонте", rayon: "Кировский", workers: 1 },
+        { id: 3, title: "Требуется слесарь", rayon: "Железнодорожный", workers: 1 },
+        { id: 4, title: "Требуется сварщик", rayon: "Ворошиловский", workers: 3 },
+        { id: 5, title: "Требуется Сиделка", rayon: "Октябрьский", workers: 4 },
+        { id: 6, title: "Ищу бригаду строителей ", rayon: "Советский", workers: 3 },
+        { id: 7, title: "Требуется специалист по клинингу", rayon: "Железнодорожный", workers: 2 }]
       ,
       loading: false,
-      filter: 'Выберите район',
-      filter2: 'Выберите количество человек'
+      filterRegion: 'Выберите район',
+      filterWorkers: 'Выберите количество человек'
     }
   },
-  
   computed: {
     filteredTodos() {
-      let todos = this.todos
-      if (this.filter === 'Выберите район') {
-        return this.todos
-      }
-
-      if (this.filter === 'Ворошиловский') {
-        return this.todos.filter(t => t.rayon==="Ворошиловский")
-      }
-
-      if (this.filter === 'Железнодорожный') {
-        return this.todos.filter(t => t.rayon==="Железнодорожный")
-      }
-       if (this.filter === 'Кировский') {
-        return this.todos.filter(t => t.rayon==="Кировский")
-      }
-
-      if (this.filter === 'Ленинский') {
-        return this.todos.filter(t => t.rayon==="Ленинский")
-      }
-       if (this.filter === 'Октябрьский') {
-        return this.todos.filter(t => t.rayon==="Октябрьский")
-      }
-
-      if (this.filter === 'Первомайский') {
-        return this.todos.filter(t => t.rayon==="Первомайский")
-      }
-       if (this.filter === 'Пролетарский') {
-        return this.todos.filter(t => t.rayon==="Пролетарский")
-      }
-
-      if (this.filter === 'Советский') {
-        return this.todos.filter(t => t.rayon==="Советский")
-      }
+      return this.todos
+          .filter(t => this.filterRegion === 'Выберите район' || t.rayon === this.filterRegion)
+          .filter(t => this.filterWorkers === 'Выберите количество человек' || t.workers == this.filterWorkers);
     },
-    filteredTodos2() {
-      if (this.filter2 === 'Выберите количество работников') {
-        return this.todos
-      }
-
-      if (this.filter2 === '1') {
-        return this.todos.filter(t => t.workers===1)
-      }
-
-      if (this.filter2 === '2') {
-        return this.todos.filter(t => t.workers===2)
-      }
-       if (this.filter2 === '3') {
-        return this.todos.filter(t => t.workers===3)
-      }
-
-      if (this.filter2 === '4') {
-        return this.todos.filter(t => t.workers===4)
-      }
-       if (this.filter2 === '5 и более') {
-        return this.todos.filter(t => t.workers>=5)
-      }
-
-    
-    }
-
   },
   methods: {
     removeTodo(id) {
@@ -154,6 +92,6 @@ select {
   display: inline;
   border-right: 500px;
   border-color: brown;
-  
+
 }
 </style>
