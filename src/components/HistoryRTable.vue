55883abd5a71a226c1e66ac102c842f085b3e882
Ajerzz <75088841+Ajerzz@users.noreply.github.com>
2021-04-07 18:46:05 +0300

    Add files via upload

---
+++ src/components/HistoryRTable.vue
@@ -0,0 +1,50 @@
+<template>
+<div>
+<div> 
+<button type="button" class="btn btn-primary"><b><i><router-link to="/addresume"><font color="white"><p class="fs-3">Добавить резюме</p></font></router-link></i></b></button></div>
+  <table class="table">
+  <thead class="table-dark">
+  
+    <tr>
+      <th>#  </th>
+      <th>Имя</th>
+      <th>Род деятельности</th>
+      <th>Возраст</th>
+      <th>Открыть</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="(worker, idx) of workers" :key="worker.id">
+      <td>{{ idx + 1 }}</td>
+      <td>{{ worker.Name }}</td>
+      <td>{{ worker.Abilities}}</td>
+      <td>{{ worker.Age}}</td>
+      <!-- <td>{{ record.categoryName }}</td> -->
+      <!-- <td>
+        <span 
+          :class="[record.typeClass]"
+          class="white-text badge"
+        >{{ record.typeText }}</span>
+      </td> -->
+      <td>
+        <button type="button" class="btn btn-success"  @click="$router.push('/detailR/' + worker.id)"> <b><i class="material-icons">Подробно</i></b></button>
+      </td>
+
+    </tr>
+  </tbody>
+</table>
+
+ 
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    workers: {
+      required: true,
+      type: Array
+    }
+  }
+}
+</script>
---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> Извините, произошла ошибка. Страница не найдена </h1> 
+
+
+        <router-link to='/'><h3>Вернуться на начальную страницу</h3></router-link>
+    </div>
+</template>
+
+
+<script>
+export default {
+ name:"PageNotFound"
+}
+</script>
