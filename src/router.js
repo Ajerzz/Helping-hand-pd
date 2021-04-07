41b7be7dd86478fbb98a99f93f5044168617e207
Ajerzz <75088841+Ajerzz@users.noreply.github.com>
2021-04-07 18:40:50 +0300

    Add files via upload

--- src/router.js
+++ src/router.js
@@ -12,11 +12,68 @@ export default new Router({
       component: Home
     },
     {
-      path: '/todos',
-      component: () => import('./views/Todos.vue')
+      path: '/Home2',
+      component: () => import('./views/Home2.vue')
     },
-    {path: '/resume',
-     component: ()=>import('./views/resume.vue')
-     }
+    // {
+    //   path: '/todos',
+    //   component: () => import('./views/Todos.vue')
+    // },
+    // {path: '/resume',
+    //  component: ()=>import('./views/resume.vue')
+    //  },
+     {
+      path: '*',
+
+      meta: {layout: 'empty'},
+      component: () => import('./components/PageNotFound.vue')
+    },
+     
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      //meta: {layout: 'main', auth: true},
+      component: () => import('./views/Detail.vue')
+    },
+    {
+      path: '/detailR/:id',
+      name: 'detailR',
+      //meta: {layout: 'main', auth: true},
+      component: () => import('./views/DetailR.vue')
+    },
+    {
+      path: '/login',
+
+      meta: {layout: 'empty'},
+      component: () => import('./views/Login.vue')
+    },
+    {
+      path: '/registration',
+    
+      meta: {layout: 'empty'},
+      component: () => import('./views/Registration.vue')
+    },
+    {
+      path: '/addresume',
+      meta: {layout: 'empty'},
+      component: () => import('./views/NewResume.vue')
+    },
+    {
+      path: '/addvacancy',
+      meta: {layout: 'empty'},
+      component: () => import('./views/NewVacancy.vue')
+    },
+    {
+      path: '/resume',
+
+      meta: {layout: 'empty'},
+      component: () => import('./views/Resume.vue')
+    },
+    {
+      path: '/vacancies',
+
+      meta: {layout: 'empty'},
+      component: () => import('./views/Vacancies.vue')
+    }
   ]
 })
