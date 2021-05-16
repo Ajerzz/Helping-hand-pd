77452cea87c5c64b64abdb258911a94b987a6d8c
Dekardexe <75323092+Dekardexe@users.noreply.github.com>
2021-05-17 00:08:42 +0300

    Не туда

--- utils/messages.js
+++
@@ -1,6 +0,0 @@
-export default {
-  'logout': 'Вы вышли из системы',
-  'auth/user-not-found': 'Пользователя с таким email не существует',
-  'auth/wrong-password': 'Неверный пароль',
-  'auth/email-already-in-use': 'Email уже занят'
-}
